b92cbd21749b5ac1b7c5cb8f96e2b755bf92fd11
Krishna Paul <reach.krishnapaul@outlook.com>
2020-05-31 00:14:55 +0530

    Employee Page calander input and UI, corresponding code-behind Updated.

--- NBU.PayBill.Salary.BL/DataAccessService/EmployeeDAS.cs
+++ NBU.PayBill.Salary.BL/DataAccessService/EmployeeDAS.cs
@@ -117,10 +117,136 @@ namespace NBU.PayBill.Salary.BL.DataAccessService
             {
                 helper.OpenConnection();
                 query = @"
-                           UPDATE [dbo].[DesignationMaster]
+                           UPDATE [dbo].[EmpMaster]
                            SET  [EmpName] = '" + employee.EmpName + @"'
-                                
+                                ,[EmpAADHAR] = '" + employee.EmpAADHAR + @"'
+                                ,[EmpBloodGroup] = '" + employee.EmpBloodGroup + @"'
+                                ,[EmpDepartmentCD] = '" + employee.EmpDepartmentCD + @"'
+                                ,[EmpDesignationCD] = '" + employee.EmpDesignationCD + @"'
+                                ,[EmpDOB] = '" + employee.EmpDOB + @"'
+                                ,[EmpDOJ] = '" + employee.EmpDOJ + @"'
+                                ,[EmpEmailId] = '" + employee.EmpEmailId + @"'
+                                ,[EmpEmergencyContactNo] = '" + employee.EmpEmergencyContactNo + @"'
+                                ,[EmpGroup] = '" + employee.EmpGroup + @"'
+                                ,[EmpIsActive] = '" + employee.EmpIsActive + @"'
+                                ,[EmpPAN] = '" + employee.EmpPAN + @"'
+                                ,[EmpPersonalContactNo] = '" + employee.EmpPersonalContactNo + @"'
+                                ,[EmpRemarks] = '" + employee.EmpRemarks + @"'
+                                ,[EmpResidentialAddress] = '" + employee.EmpResidentialAddress + @"'
+                                ,[EmpSex] = '" + employee.EmpSex + @"'
+                                ,[EmpCaste] = '" + employee.EmpCaste + @"'
+                                ,[EmpDOR] = '" + employee.EmpDOR + @"'
+                                ,[EmpDNI] = '" + employee.EmpDNI + @"'
+                                ,[EmpMaritalStatus] = '" + employee.EmpMaritalStatus + @"'
+                                ,[EmpQuarterNumber] = '" + employee.EmpQuarterNumber + @"'
+                                ,[EmpQualification] = '" + employee.EmpQualification + @"'
+                                ,[EmpReligion] = '" + employee.EmpReligion + @"'
+                                ,[UpdateTMS] = '" + DateTime.Now.ToString() + @"'
+                                ,[UpdateUser] = 'testing'
                            WHERE [EmpID] LIKE '" + employee.EmpID + "'";
+
+                numberOfRowImpacted = helper.ExecuteNonQuery(query);
+            }
+            catch (Exception ex)
+            {
+                numberOfRowImpacted = -1;
+            }
+            finally
+            {
+                helper.CloseConnection();
+            }
+
+            return numberOfRowImpacted;
+        }
+
+        public int AddEmployee(EmployeeBO employee)
+        {
+            SQLHelper helper = SQLHelper.GetInstance(this.connectionString);
+            int numberOfRowImpacted = 0;
+            try
+            {
+                helper.OpenConnection();
+                query = @"
+                          INSERT INTO [dbo].[EmpMaster]
+                                ([EmpID]
+                                ,[EmpAADHAR]
+                                ,[EmpBloodGroup]
+                                ,[EmpDepartmentCD]
+                                ,[EmpDesignationCD]
+                                ,[EmpDOB]
+                                ,[EmpDOJ]
+                                ,[EmpEmailId]
+                                ,[EmpEmergencyContactNo]
+                                ,[EmpGroup]
+                                ,[EmpIsActive]
+                                ,[EmpName]
+                                ,[EmpPAN]
+                                ,[EmpPersonalContactNo]
+                                ,[EmpRemarks]
+                                ,[EmpResidentialAddress]
+                                ,[EmpSex]
+                                ,[EmpCaste]
+                                ,[EmpDOR]
+                                ,[EmpDNI]
+                                ,[EmpMaritalStatus]
+                                ,[EmpQuarterNumber]
+                                ,[EmpQualification]
+                                ,[EmpReligion]
+                                ,[UpdateTMS]
+                                ,[UpdateUser])
+						VALUES
+								(CAST((SELECT TOP 1 e.[EmpID] FROM [dbo].[EmpMaster] as e ORDER BY CAST(e.[EmpID] as int) DESC)+1 as varchar)
+                                , '" + employee.EmpAADHAR + @"'
+                                , '" + employee.EmpBloodGroup + @"'
+                                , '" + employee.EmpDepartmentCD + @"'
+                                , '" + employee.EmpDesignationCD + @"'
+                                , '" + employee.EmpDOB + @"'
+                                , '" + employee.EmpDOJ + @"'
+                                , '" + employee.EmpEmailId + @"'
+                                , '" + employee.EmpEmergencyContactNo + @"'
+                                , '" + employee.EmpGroup + @"'
+                                , '" + employee.EmpIsActive + @"'
+                                , '" + employee.EmpName + @"'
+                                , '" + employee.EmpPAN + @"'
+                                , '" + employee.EmpPersonalContactNo + @"'
+                                , '" + employee.EmpRemarks + @"'
+                                , '" + employee.EmpResidentialAddress + @"'
+                                , '" + employee.EmpSex + @"'
+                                , '" + employee.EmpCaste + @"'
+                                , '" + employee.EmpDOR + @"'
+                                , '" + employee.EmpDNI + @"'
+                                , '" + employee.EmpMaritalStatus + @"'
+                                , '" + employee.EmpQuarterNumber + @"'
+                                , '" + employee.EmpQualification + @"'
+                                , '" + employee.EmpReligion + @"'
+                                , '" + DateTime.Now.ToString() + @"'
+                                , 'testing')";
+
+                numberOfRowImpacted = helper.ExecuteNonQuery(query);
+            }
+            catch (Exception ex)
+            {
+                numberOfRowImpacted = -1;
+            }
+            finally
+            {
+                helper.CloseConnection();
+            }
+
+            return numberOfRowImpacted;
+        }
+
+        public int DeleteEmployee(string EmpID)
+        {
+            SQLHelper helper = SQLHelper.GetInstance(this.connectionString);
+            int numberOfRowImpacted = 0;
+            try
+            {
+                helper.OpenConnection();
+                query = @"
+                           DELETE FROM [dbo].[EmpMaster]
+                                WHERE [EmpID] LIKE '"
+                                + EmpID + "'";
 
                 numberOfRowImpacted = helper.ExecuteNonQuery(query);
             }
